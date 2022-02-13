857e8e2212ea11fd0d637cb4e011fe05c05a8d5b
Michal Wakulinski <michal.wakulinski1@gmail.com>
2022-02-13 19:14:06 +0100

    second commit - update Cart class

--- cart-item.js
+++ cart-item.js
@@ -3,66 +3,88 @@ const Validator = require("./validator");
 
 class CartItem {
   #id;
-  #primaryPrice;
-  constructor(name, category, price) {
+  constructor(name, price, category) {
     this._name = name;
-    this.category = [...category, "All"];
+    this._categories = [...category, name, "All"];
     this._price = price;
-    this.#primaryPrice = price;
     this.discount = 0;
     this.#id = uuidv4();
-    this.discountCode = [];
+    this.discounts = [];
   }
 
-  set _name(value) {
-    if (typeof value === "string") {
-      this.name = value;
-    }
+  set _name(name) {
+    Validator.throwIfNotProperType(name, "string");
+    this.name = name;
   }
 
-  set _price(value) {
-    if (typeof value === "number") {
-      this.price = value;
-    }
+  set _categories(category) {
+    Validator.throwIfNotArr(category);
+    Validator.checkIfHaveOnlyString(category);
+    this.categories = [...category];
   }
+
+  set _price(price) {
+    Validator.throwIfNumbrNonPositive(price);
+    this.price = price;
+  }
+
+  // set _amount(amount) {
+  //   Validator.throwIfNumbrNonPositive(amount);
+  //   Validator.throwIfNotInt(amount);
+  //   this.price = price;
+  // }
 
   get id() {
     return this.#id;
   }
 
   addItemCategory(category) {
-    //czu juz jest w tej kategorii
-    Validation.throwIfNotString(category);
-    this.category.push(category);
+    Validator.throwIfNotProperType(category, "string");
+    this.throwIfProductHaveThisCategory(category);
+    this.categories.push(category);
   }
 
   setDiscountCode(discount) {
-    if (!this.discountCode.some((item) => item === discount.name)) {
-      this.discountCode.push(discount.name);
+    if (
+      !this.isDiscountUsed(discount) &&
+      this.chechIfProductHaveDiscountCategory(discount)
+    ) {
+      this.discounts.push(discount);
     }
   }
 
+  calculateItemPrice() {
+    this.price *= 1 - this.discount / 100;
+  }
   //ustala cene po znizce, ustala jaka jest procentowa znizka i dodaje znizke do uzytych
   //calculatePrice()
   //{valueInPercent: number, category: string, name}
-  setItemDiscount(discount) {
-    //checkIsProductHaveDiscountCategory()
-    //
-    /*
-    const isInArray = Utilities.findInArrByCategory(
-      this.category,
-      discount.category
+
+  calculateItemDiscount() {
+    const discountValueArr = this.discounts.map(
+      (discount) => discount.valueInPrecent
     );
-    if (
-      Utilities.findInArrByCategory(this.category, discount.category) &&
-      Utilities.doesNotExistInArr(this.discountCode, discount.name)
-    ) {
-      this.price = this.price * (1 - discount.value / 100);
-      this.discount = `${100 - (this.price / this.#primaryPrice) * 100}%`;
-      this.addUsedDiscountCode(discount);
+
+    this.discount =
+      100 -
+      discountValueArr.reduce((total, currnet) => {
+        return total * ((100 - currnet) / 100);
+      }, 100);
+  }
+
+  isDiscountUsed(discountCode) {
+    return this.discounts.some((discount) => discount === discountCode);
+  }
+
+  chechIfProductHaveDiscountCategory(discount) {
+    return this.categories.some((category) => category === discount.category);
+  }
+
+  throwIfProductHaveThisCategory(newCategory) {
+    if (this.categories.some((category) => category === newCategory)) {
+      throw new Error(`This item already is in ${newCategory} category`);
     }
-  }*/
   }
 }
 
-module.exports = { CartItem, Cart };
+module.exports = CartItem;
